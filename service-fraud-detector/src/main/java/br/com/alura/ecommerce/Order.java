dfceab33b3b08bb5469f20b4119d6d46f162c2c1
PauloGuedes2 <ph.guedes99@outlook.com>
2021-03-11 22:09:01 -0300

    Produtores consumidores e o eager de patterns

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/Order.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/Order.java
@@ -1,14 +1,15 @@
 package br.com.alura.ecommerce;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
+@ToString
+@Getter
 public class Order {
     private final String userId, orderId;
     private final BigDecimal amount;
-
-    public Order(String userId, String orderId, BigDecimal amount) {
-        this.userId = userId;
-        this.orderId = orderId;
-        this.amount = amount;
-    }
 }
--- service-new-order/src/main/java/br/com/alura/ecommerce/Order.java
+++ service-new-order/src/main/java/br/com/alura/ecommerce/Order.java
@@ -1,14 +1,12 @@
 package br.com.alura.ecommerce;
 
+import lombok.AllArgsConstructor;
+
 import java.math.BigDecimal;
 
+@AllArgsConstructor
 public class Order {
     private final String userId, orderId;
     private final BigDecimal amount;
 
-    public Order(String userId, String orderId, BigDecimal amount) {
-        this.userId = userId;
-        this.orderId = orderId;
-        this.amount = amount;
-    }
 }
